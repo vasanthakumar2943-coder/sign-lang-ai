164372ba397dc6ab961cb9be8453392ce048a5dd
Vasanth <vasanthakumar2943@gmail.com>
2026-01-22 15:44:59 +0530

    Fix monorepo structure for Vercel

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";   // 🔥 REQUIRED
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
-);
